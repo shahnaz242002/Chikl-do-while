7f8a6ea29c1170efaa91fc7b20007f396b2f4b28
shahnaz242002 <shahnazavazova@gmail.com>
2023-04-03 16:36:13 +0400

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Напишите свой пароль");
+int parol1 = Convert.ToInt32(Console.ReadLine());
+int parol2 = Convert.ToInt32(Console.ReadLine());
+if (parol1 == parol2) 
+{
+    Console.WriteLine("Пароли совпадают");
+}
+else
+{
+    do
+    {
+        Console.WriteLine("Пароли не совпадают");
+        parol1 = Convert.ToInt32(Console.ReadLine());
+        parol2 = Convert.ToInt32(Console.ReadLine());
+
+    }
+    while (parol1 != parol2);
+
+}
+
+
